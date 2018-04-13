6120f4f9715a60ba841771e8a15a7f4427804554
TDXL <420182279@qq.com>
2018-04-13 20:54:39 +0800

    表单登录-axios+element

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <p>app components</p>
+    <!-- <p>app components</p> -->
     <router-view/>
   </div>
 </template>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,49 @@
 <template>
-<div>
-  <p>login components</p>
+<div class="login-wrap">
+  <el-form label-position="top" ref="form" :model="userForm" label-width="80px">
+    <el-form-item label="用户名">
+      <el-input v-model="userForm.username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input
+      type="password"
+      v-model="userForm.password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="login">立即创建</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
-    return {}
+    return {
+      userForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login () {
+      // 数据
+      // 验证
+      // 请求
+      // 交互
+      // console.log(this.userForm)
+      const res = await axios.post('http://localhost:8888/api/private/v1/login', this.userForm)
+      // console.log(res)
+      const data = res.data
+      if (data.meta.status === 200) {
+        this.$router.push({
+          name: 'home'
+        })
+      }
+    }
   }
 }
 </script>
