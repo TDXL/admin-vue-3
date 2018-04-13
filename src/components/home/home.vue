8de3e46d5578c4e2bee4c21f1fee55ccb8ced90f
TDXL <420182279@qq.com>
2018-04-13 19:57:31 +0800

    新建组件 配置路由

---
+++ src/components/home/home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <p>home components</p>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <p>login components</p>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
