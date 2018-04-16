b1eae1a8e852f2c44159e2e8f3f56f886c1ae841
TDXL <420182279@qq.com>
2018-04-16 20:06:37 +0800

    用户列表简单渲染-请求&钩子&获取token&配置请求头&请求参数

--- src/components/user-list/user-list.vue
+++ src/components/user-list/user-list.vue
@@ -13,7 +13,7 @@
   <!-- 搜索框 -->
   <el-row class="user-list-search">
     <el-col :span="8">
-      <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
+      <el-input placeholder="请输入内容" v-model="searchText" class="input-with-select">
         <el-button slot="append" icon="el-icon-search"></el-button>
       </el-input>
     </el-col>
@@ -26,18 +26,18 @@
     :data="tableData"
     style="width: 100%">
     <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
+      prop="username"
       label="姓名"
       width="180">
     </el-table-column>
     <el-table-column
-      prop="address"
-      label="地址">
+      prop="email"
+      label="邮箱"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="mobile"
+      label="电话">
     </el-table-column>
   </el-table>
   <!-- 分页 -->
@@ -54,27 +54,26 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  async created () {
+    const {token} = JSON.parse(window.localStorage.getItem('admin-token'))
+    const res = await axios.get('http://localhost:8888/api/private/v1/users', {
+      headers: {  //配置请求头携带身份令牌
+        Authorization: token
+      },
+      params: {  //参数
+        pagenum: 1,
+        pagesize: 5
+      }
+    })
+    // console.log(res)
+    this.tableData = res.data.data.users
+  },
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
-      input5: ''
+      tableData: [],
+      searchText: ''
     }
   },
   methods: {
